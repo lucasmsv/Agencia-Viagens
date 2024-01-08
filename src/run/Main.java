2ef0f7e9cefd1784d273bb8248e186cf0fecd715
Lucas <>
2024-01-08 09:47:37 -0300

    Definindo tela inicial do projeto

--- src/run/Main.java
+++ src/run/Main.java
@@ -1,10 +1,17 @@
 package run;
 
+import view.ViewInicial;
+
 public class Main {
 
     public static void main(String[] args) {
         
-        // 
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ViewInicial().setVisible(true);
+            }
+        });
         
     }
     
